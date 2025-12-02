c64e9e362a58cc996ee36a705ee698d1993e6c8b
merybist <86554244+merybist@users.noreply.github.com>
2025-12-02 15:17:15 -0800

    Refactor download process in merybist.ps1
    Refactor download logic to check for existing files before downloading. Improved error handling during downloads.

--- merybist.ps1
+++ merybist.ps1
@@ -22,26 +22,30 @@ $apps = @(
 )
 
 foreach ($app in $apps) {
-    Write-Host "`nDownloading $($app.name)..."
     $path = "$env:TEMP\$($app.name).exe"
-    
-    try {
-        Invoke-WebRequest $app.url -OutFile $path -UseBasicParsing
-        Write-Host "Downloaded $($app.name)."
+
+    if (Test-Path $path) {
+        Write-Host "`nSkipping download for $($app.name), file already exists."
     }
-    catch {
-        Write-Host "ERROR downloading $($app.name): $_"
-        continue
+    else {
+        Write-Host "`nDownloading $($app.name)..."
+        try {
+            Invoke-WebRequest $app.url -OutFile $path -UseBasicParsing
+            Write-Host "Downloaded $($app.name)."
+        }
+        catch {
+            Write-Host "ERROR downloading $($app.name): $_"
+            continue
+        }
     }
 
     Write-Host "Installing $($app.name)..."
-
     try {
         if ($app.args -ne "") {
             Start-Process $path -ArgumentList $app.args -Wait
         }
         else {
-            Start-Process $path
+            Start-Process $path -Wait
         }
     }
     catch {
