c61b77fb6331d5b4957d0c098cda3e3bc33757e7
merybist <86554244+merybist@users.noreply.github.com>
2026-01-19 18:24:37 +0200

    Update merybist.ps1

--- merybist.ps1
+++ merybist.ps1
@@ -8,10 +8,10 @@ $apps = @(
         InstallPath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
     },
     @{ 
-        Name = "7-zip"
-        Url = "https://github.com/merybist/merybist-scripts/raw/refs/heads/main/Soft/7-zip.exe"
-        Args = ""
-        InstallPath = "C:\Program Files\7-Zip\7zFM.exe"
+        Name = "WinRAR"
+        Url = "https://github.com/merybist/merybist-scripts/raw/refs/heads/main/Soft/winrar.exe"
+        Args = "/S"
+        InstallPath = "C:\Program Files\WinRAR\WinRAR.exe"
     },
     @{ 
         Name = "iMe | DirectLink"
@@ -49,20 +49,8 @@ $apps = @(
         Args = "/quiet"
         InstallPath = "C:\Program Files\Tailscale\tailscale.exe"
     },
-        @{
-        Name = "Itunes | DirectLink"
-        Url = "https://www.apple.com/itunes/download/win64"
-        Args = ""
-        InstallPath = "C:\Program Files\Itunes\iTunes.exe"
-    },
     @{ 
-        Name = "WinRAR"
-        Url = "https://github.com/merybist/merybist-scripts/raw/refs/heads/main/Soft/winrar.exe"
-        Args = "/S"
-        InstallPath = "C:\Program Files\WinRAR\WinRAR.exe"
-    },
-    @{ 
-        Name = "WinRAR Activation"
+        Name = "WinRAR Key"
         Url = "https://github.com/merybist/merybist-scripts/raw/refs/heads/main/Soft/rarreg.key"
         Args = ""
         InstallPath = "C:\Program Files\WinRAR\rarreg.key"
